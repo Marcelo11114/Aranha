cf6bf1eb66bc603101481586a7a69c8610a6d132
Marcelo11114 <dedosearte@hotmail.com>
2020-09-13 15:30:33 -0300

    aranha, esqueci como colocar um caracter sem usar write , mas deu certo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aranha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Spider");
+            Console.WriteLine(" ****** \n");
+
+            Console.WriteLine(" ||  ||"); 
+            Console.Write(" \\");
+            Console.WriteLine("\\()// ");
+            Console.Write("//(__)\\");
+            Console.WriteLine("\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
